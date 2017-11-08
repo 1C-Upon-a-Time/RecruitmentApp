6bbea8d496bc84a2053bdbc9b807ca09f5bdf53f
Luke Bickell <luke@bickell.com>
2017-11-08 15:45:57 -0500

    confirm student for interview

--- modules/calendar/client/controllers/interviewSlots.client.controller.js
+++ modules/calendar/client/controllers/interviewSlots.client.controller.js
@@ -1,19 +1,26 @@
 'use strict';
-angular.module('calendar').controller('SlotsController', ['$scope', '$location', '$stateParams', '$state', '$http', 'InterviewSlots',
-  function($scope, $location, $stateParams, $state, $http, InterviewSlots){
-    $scope.interviews = "";
+angular.module('calendar').controller('SlotsController', ['$scope', '$location', '$stateParams', '$state', '$http', 'InterviewSlots', 'Students',
+  function($scope, $location, $stateParams, $state, $http, InterviewSlots, Students){
+
     // MOST OF THESE SHOULD BE ROOTSCOPE DEFINED FROM AN ADMIN LEVEL CONTROLLER
     $scope.hours = [8,9,10,11,13,14,15,16];
     $scope.slots = [1,2,3];
     $scope.numRecruiters = 3;
-    $scope.day = new Date(2017,9,3);
+    $scope.day = new Date(2017,11,4);
     $scope.interviewArrays = [];
     $scope.selectingStudentInterview = false;
     if ($stateParams.studentId)
     {
         $scope.selectingStudentInterview = true;
+        $scope.studentId = $stateParams.studentId;
     }
 
+    $scope.init = function(){
+        $scope.getInterviews();
+        if ($scope.selectingStudentInterview) {
+            $scope.findOne();
+        }
+    }
     // Gets all the interview slots.
     $scope.getInterviews = function() {
       /* set loader*/
@@ -32,59 +39,57 @@ angular.module('calendar').controller('SlotsController', ['$scope', '$location',
 
     };
 
-    // Hacky filter for table so that it displays the slots in the correct time
-    // $scope.slotFilter = function(hour)
-    // {
-    //     return function(interview)
-    //     {
-    //         var interviewHour = new Date(interview.date).getHours();
-    //         return hour===interviewHour;
-    //     };
+    $scope.findOne = function() {
+      //debugger;
+      $scope.loading = true;
 
-    // };
+      var id = $stateParams.studentId;
 
-    // $scope.formatInterviewDates = function(){
-    //     console.log($scope.interviews.length);
-    //   for (var i = 0; i < $scope.interviews.length; i++)
-    //   {
-    //     console.log("test 1");
-    //     var date = new Date($scope.interviews[i].date);
-    //     //$scope.interviews[i].date = date;
-    //     $scope.interviews[i].hour = date.getHours();
-    //     console.log("test 2");
-    //     console.log("a " + $scope.interviews[i].date.getHours());
-    //   }
-    // }
-
-    // Just calls the update method
-    $scope.selectInterview = function(interview) {
-        console.log(interview._id);
-        if ($stateParams.studentId)
-        {
-            console.log("student: " + $stateParams.studentId);
-        }
-        $scope.update(interview);
+      Students.read(id)
+      .then(function(response) {
+        $scope.student = response.data;
+        $scope.loading = false;
+      }, function(error) {  
+        $scope.error = 'Unable to retrieve student with id "' + id + '"\n' + error;
+        $scope.loading = false;
+      });
     };
 
-    // // THIS METHOD SHOULD BE PART OF THE ADMIN CONTROLLER
-    // $scope.createDayInterviewSlots = function(day, numRecruiters, hours) {
-    //     // SET GLOBAL "DAY" VARIABLE
+    // Just calls the update method
+    $scope.selectForInterview = function(interview) {
+        var date = new Date(interview.date);
+        var confirmText = "Confirm " + $scope.student.name + " for " + date + " and send email to " + $scope.student.email + "?";
+        if (confirm(confirmText))
+        {
+            $scope.update(interview);
+            alert("Interview scheduled!");
+        }
+    };
 
-    //     for (var i = 0; i < hours.length; i++)
-    //     {
-    //         for (var j = 0; j < numRecruiters; j++)
-    //         {
-    //             var interviewSlot = {
-    //                 // Date setHours is 0-23
-    //                 date: day.setHours(hours[i-1]),
-    //                 slot: j+1,
-    //             };
+    // THIS METHOD SHOULD BE PART OF THE ADMIN CONTROLLER
+    $scope.createDayInterviewSlots = function() {
+        // SET GLOBAL "DAY" VARIABLE
+        var day = new Date(2017,11,4);
+        var numRecruiters = 3;
+        var hours = [8,9,10,11,13,14,15,16];
+        console.log(day);
+        console.log(numRecruiters);
+        console.log(hours);
+        for (var i = 0; i < hours.length; i++)
+        {
+            for (var j = 0; j < numRecruiters; j++)
+            {
+                var interviewSlot = {
+                    // Date setHours is 0-23
+                    date: day.setHours(hours[i]-1),
+                    slot: j+1
+                };
 
-    //             InterviewSlots.create(interviewSlot);
-    //             console.log("Slot created!");
-    //         }
-    //     }
-    // };
+                InterviewSlots.create(interviewSlot);
+                console.log("Slot created!");
+            }
+        }
+    };
 
     // Reverses the availability of the slot selected
     $scope.update = function(interview) {
