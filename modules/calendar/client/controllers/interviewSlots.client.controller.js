7609ffd28139d8b3a889556f259a432af8b2c582
Luke Bickell <luke@bickell.com>
2017-11-14 10:04:42 -0500

    interview now displays on candidate profile page

--- modules/calendar/client/controllers/interviewSlots.client.controller.js
+++ modules/calendar/client/controllers/interviewSlots.client.controller.js
@@ -106,8 +106,7 @@ angular.module('calendar').controller('SlotsController', ['$scope', '$location',
         interviewSlot.student = $scope.student._id;
 
         updatedStudent.interview = interviewSlot._id;
-        console.log("I " +interviewSlot.student);
-        console.log("S " +updatedStudent.interview);
+        
         /* Save the article using the Listings factory */
         InterviewSlots.update(slot_id, interviewSlot)
         .then(function(response) {
